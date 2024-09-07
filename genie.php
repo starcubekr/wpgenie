ee0b5a09fe01331cb23570b77bce26e236620469
starcubekr <80164970+starcubekr@users.noreply.github.com>
2024-09-07 19:55:01 +0900

    Initial Import

---
+++ genie.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name:       Genie
+ * Description:       Book info display with Aladin OPENAPI.
+ * Requires at least: 6.6
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            Seokmoon, JANG
+ * License:           MIT
+  * Text Domain:       genie
+ *
+ * @package CreateBlock
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function create_block_genie_block_init() {
+	register_block_type( __DIR__ . '/build' );
+}
+add_action( 'init', 'create_block_genie_block_init' );
